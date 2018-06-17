a644804dbd9792b4b5cf919931feffccf2867b94
Alex Ford <fordas@uw.edu>
2018-06-16 23:34:36 -0700

    Cleanup webhook secret resolution, fix tests.

--- ghapp/ghapp/buildkite/webhooks.py
+++ ghapp/ghapp/buildkite/webhooks.py
@@ -1,3 +1,6 @@
+from typing import Optional
+import os
+
 import logging
 
 import attr
@@ -10,7 +13,32 @@ logger = logging.getLogger(__name__)
 
 @attr.s(auto_attribs=True)
 class BuildkiteHooks:
-    token: str
+    SECRET_ENV_VAR = "BUILDKITE_WEBHOOK_SECRET"
+
+    @staticmethod
+    def _resolve_secret(secret: Optional[str] = None):
+        """Resolve secret from env or target file, falling back to `GITHUB_WEBHOOK_SECRET`."""
+        if secret is None:
+            logger.debug("Resolving secret from env.")
+
+            secret = os.getenv(BuildkiteHooks.SECRET_ENV_VAR)
+            if secret is None:
+                raise ValueError("Unable to resolve secret from env: %s" %
+                                 BuildkiteHooks.SECRET_ENV_VAR)
+            logger.info("Resolved %s to secret.",
+                        BuildkiteHooks.SECRET_ENV_VAR)
+
+        if os.path.isfile(secret):
+            logger.info("Resolved secret to filename: %s", secret)
+            secret = open(secret, "r").read()
+
+        logger.debug("Resolved secret.", secret)
+
+        return secret
+
+    secret: bytes = attr.ib(
+        converter=_resolve_secret.__func__,
+        default=attr.Factory(lambda: BuildkiteHooks._resolve_secret()))
 
     signals: SignalSet = attr.Factory(SignalSet)
 
@@ -18,9 +46,9 @@ class BuildkiteHooks:
         # Get and validate signature
         token = req.headers.get('x-buildkite-token')
         if token:
-            if not token == self.token:
+            if not token == self.secret:
                 logging.debug("x-buildkite-token: %s", token)
-                logging.debug("token: %s", self.token)
+                logging.debug("secret: %s", self.secret)
                 return web.Response(status=401, text="invalid x-buildkite-token")
 
         # Get body, only application/json
